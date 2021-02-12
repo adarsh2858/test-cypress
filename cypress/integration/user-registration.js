6a4ef825443b4d779eba947458f914050b5bbf8d
ash2858 <adarsh@cloudyuga.guru>
2021-02-12 13:46:01 +0530

    add the user registration form testing workflow

---
+++ cypress/integration/user-registration.js
@@ -0,0 +1,26 @@
+describe("User registration process", () => {
+  it("fills in the details on the sign up page", () => {
+    cy.visit("/");
+    cy.contains("Sign Up").click();
+
+    const password = Cypress.env("user_password");
+
+    cy.get('input[name="user[email]"]')
+      .type("adarsh2858@gmail.com")
+      .should("have.value", "adarsh2858@gmail.com");
+    cy.get('input[name="user[first_name]"]')
+      .type("John")
+      .should("have.value", "John");
+    cy.get('input[name="user[last_name]"]')
+      .type("Doe")
+      .should("have.value", "Doe");;
+
+    cy.scrollTo('center').wait(500);
+
+    cy.get('input[name="user[password]"]').type(password);
+
+    // Work on bypassing the captcha verification during the sign up process
+    // cy.get('span[role="checkbox"]').click()
+    // cy.get('div[class="recaptcha-checkbox-border"]').click()
+  });
+});
