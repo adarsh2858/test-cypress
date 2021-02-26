510b46aa975addce92a0748983cad186bd5c9353
ash2858 <adarsh@cloudyuga.guru>
2021-02-26 16:27:29 +0530

    add the workflow to test search for course on the homepage

---
+++ cypress/integration/search-for-course.js
@@ -0,0 +1,22 @@
+describe("Search for a course", () => {
+  it("searches for a course", () => {
+    cy.visit("/");
+    cy.get("#all-courses").scrollIntoView().wait(1000);
+
+    cy.get("input[placeholder='Search Courses']").type("test");
+
+    cy.get(".search-courses")
+      .children()
+      .then(($child) => {
+        if ($child.children().length > 0) {
+          let totalCourses = $child.children().length;
+
+          while (totalCourses) {
+            cy.get(
+              `.featured-courses > :nth-child(${totalCourses--}) > .featured-card > .px-4 > .font-display`
+            ).contains("Test", { matchCase: false });
+          }
+        } else console.log("No courses found");
+      });
+  });
+});
