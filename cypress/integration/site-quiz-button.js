9a4a04eec72285f6d5a4b75a07445fab4f440809
adarsh2858 <adarsh2858@gmail.com>
2021-03-11 12:49:37 +0530

    login as admin then student and verify the take quiz button

--- cypress/integration/site-quiz-button.js
+++ cypress/integration/site-quiz-button.js
@@ -1,10 +1,37 @@
-describe("Testing site quiz", () => {
-  it("clicks on the site quiz button", () => {
+import { userLogin } from './Utils';
+
+const adminEmail = "adarsh@cloudyuga.guru";
+const studentEmail = "nilam2267@gmail.com";
+
+describe("Testing site quiz button on the homepage", () => {
+  it("clicks on the site quiz button as a new user", () => {
     cy.visit("/explore");
     cy.contains("Quizzes").scrollIntoView().wait(1000);
 
     cy.contains("Take Quiz").click()
 
     cy.url().should("include", "/devise/users/sign_in")
+  });
+
+  it("clicks on the site quiz button as an admin", () => {
+    userLogin(adminEmail);
+
+    cy.visit("/explore");
+    cy.contains("Quizzes").scrollIntoView().wait(1000);
+
+    cy.contains("Take Quiz").click()
+
+    cy.url().should("include", "/site_quizzes")
+  });
+
+  it.only("clicks on the site quiz button as a student", () => {
+    userLogin(studentEmail);
+
+    cy.visit("/explore");
+    cy.contains("Quizzes").scrollIntoView().wait(1000);
+
+    cy.contains("Take Quiz").click()
+
+    cy.url().should("include", "/site_quizzes")
   });
 });
